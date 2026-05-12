96b6b824159a0661108d95eaf1793164d211b36c
Nelson <nelsonnaonao@gmail.com>
2026-05-12 17:05:12 -0400

    fix cors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketio = require('socket.io');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', (socket) => {
     console.log('✅ Motorizado conectado:', socket.id);
