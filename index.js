3f7181d5ca5ebf553828af2067f6a4a165cb22fe
Nelson <nelsonnaonao@gmail.com>
2026-05-12 16:27:49 -0400

    primer subida

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on('connection', (socket) => {
+    console.log('✅ Motorizado conectado:', socket.id);
+    
+    socket.on('nuevo-servicio', (data) => {
+        console.log('🚀 ¡NUEVA CARRERA DETECTADA!', data);
+        io.emit('notificar-motorizado', data);
+    });
+});
+
+server.listen(process.env.PORT || 4000, '0.0.0.0', () => {
+    console.log('📡 Antena Ruta ON encendida en puerto', process.env.PORT || 4000);
+});
